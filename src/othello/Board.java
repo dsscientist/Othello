a33e1f1c4477f35dbf7f4b95b20b3b8300934d8b
shirdav18 <shirdav18@SHS-PLAB-86776.shs.isd.issaquah.wednet.edu>
2018-05-16 11:42:19 -0700

    made a board and added variable

---
+++ src/othello/Board.java
@@ -0,0 +1,16 @@
+package othello;
+
+
+public class Board {
+    private boolean[][] gameBoard = new boolean[8][8];
+
+    public boolean[][] getGameBoard() {
+        return gameBoard;
+    }
+
+    public void setGameBoard(boolean[][] gameBoard) {
+        this.gameBoard = gameBoard;
+    }
+    
+    
+}
--- src/othello/MainFrame.java
+++ src/othello/MainFrame.java
@@ -28,16 +28,17 @@ public class MainFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(1000, 1000));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1000, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 1000, Short.MAX_VALUE)
         );
 
         pack();
